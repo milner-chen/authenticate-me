07b8c18be4510f24d4a3b133e0afdb1918e04f60
Milner Chen <milnerchen0@gmail.com>
2023-03-27 21:20:57 -0400

    backend setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# backend snake to frontend camel
+Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true
