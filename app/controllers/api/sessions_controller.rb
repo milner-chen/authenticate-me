aa235bf9998d882c96ffc6d68347e8af59c84f2b
Milner Chen <milnerchen0@gmail.com>
2023-03-28 00:42:39 -0400

    jbuilder views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,9 @@
 class Api::SessionsController < ApplicationController
   def show
     if current_user
-      render json: { user: current_user }
+      @user = current_user
+      # render json: { user: current_user }
+      render 'api/users/show'
     else
       render json: { user: nil }
     end
@@ -11,7 +13,8 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:credential], params[:password])
     if @user
       login!(@user)
-      render json: { user: @user }
+      # render json: { user: @user }
+      render 'api/users/show'
     else
       render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
     end
